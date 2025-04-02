f9769e1ddb1fca590307b73b4c3ee9998f60ab04
meghamarwarapy <megha.marwarpay@gmail.com>
2025-04-02 17:57:26 +0530

    testweb

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,11 +1,70 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { accessConstent } from "../helpingFile";
+// import { Navigate, Outlet } from "react-router-dom";
+// import { accessConstent } from "../helpingFile";
+
+// const PrivateRoute = () => {
+//   const accessToken = sessionStorage.getItem(accessConstent);
+
+//   // Check if the user is authenticated
+//   return accessToken ? <Outlet /> : <Navigate to="/login" />;
+// };
+
+// export default PrivateRoute;
+
+
+// import { Navigate, Outlet, useLocation } from "react-router-dom";
+// import { useEffect } from "react";
+
+// const accessConstent = "accessToken"; // Ensure this matches your session storage key
+
+// const PrivateRoute = () => {
+//   const location = useLocation();
+  
+//   const params = new URLSearchParams(location.search);
+//   const queryToken = params.get("accessToken");
+//   const storedToken = sessionStorage.getItem(accessConstent);
+
+//   useEffect(() => {
+//     if (queryToken) {
+//       sessionStorage.setItem(accessConstent, queryToken);
+//     }
+//   }, [queryToken]);
+
+//   const accessToken = queryToken || storedToken;
+
+//   return accessToken ? <Outlet /> : <Navigate to="/login" />;
+// };
+
+// export default PrivateRoute;
+
+
+
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const accessConstent = "accessToken";
 
 const PrivateRoute = () => {
-  const accessToken = sessionStorage.getItem(accessConstent);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  // Check if the user is authenticated
+  const params = new URLSearchParams(location.search);
+  const queryToken = params.get("accessToken");
+  const storedToken = sessionStorage.getItem(accessConstent);
+
+  useEffect(() => {
+    if (queryToken) {
+      sessionStorage.setItem(accessConstent, queryToken);
+      
+      // Remove token from URL without reloading the page
+      const newUrl = location.pathname;
+      navigate(newUrl, { replace: true });
+    }
+  }, [queryToken, navigate, location.pathname]);
+
+  const accessToken = storedToken || queryToken;
+
   return accessToken ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
+
